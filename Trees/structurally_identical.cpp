8c6e43f413472c79139a869acb751615bddc9930
Lovi Stark <54571399+lovi-bhatia@users.noreply.github.com>
2022-09-01 18:01:12 +0530

    Add files via upload

---
+++ Trees/structurally_identical.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+bool areIdentical(TreeNode<int> *root1, TreeNode<int> * root2) {
+
+    if(root1->data != root2->data)
+        return false;
+
+    if(root1->children.size()!=root2->children.size())
+        return false;
+
+    bool ans = true;
+    for(int i=0;i<root1->children.size();i++){
+        bool smallAns = areIdentical(root1->children[i],root2->children[i]);
+        if(smallAns == false)
+            return false;
+    }
+    return ans;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root1 = takeInputLevelWise();
+    TreeNode<int>* root2 = takeInputLevelWise();
+    cout << (areIdentical(root1, root2) ? "true" : "false");
+}
